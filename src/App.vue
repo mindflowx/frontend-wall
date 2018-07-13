ebf3cba1db1f25196bfe2807f3e79521f6295d13
Robert Larsson <robertlarsson@Roberts-MacBook-Pro.local>
2018-07-13 13:44:42 +0200

    Changed to use vue-service web-component distributor

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Verdana', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import wrap from '@vue/web-component-wrapper'
+// import wrap from '@vue/web-component-wrapper'
 
-const CustomElement = wrap(Vue, App);
+// const CustomElement = wrap(Vue, App);
 
-window.customElements.define('mindflowx-wall', CustomElement);
+// window.customElements.define('mindflowx-wall', CustomElement);
 
 
 
 // import Vue from 'vue'
 // import App from './App.vue'
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App)
-// }).$mount('#app')
-
-
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
